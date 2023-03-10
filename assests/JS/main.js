124aebd422eb144ec32188055ab52581571cc68a
Ana Julia M Martins <ana_juh17@hotmail.com>
2023-03-10 10:20:28 -0300

    Changes on footer

--- assests/JS/main.js
+++ assests/JS/main.js
@@ -1,17 +1,35 @@
-btChange = document.getElementById('btChange');
-btChange.addEventListener("click", changeTheme);
+const btChange = document.getElementById('btChange');
 const body = document.getElementsByTagName('body')[0];
+const main = document.getElementsByTagName('main')[0];
 const h1 = document.getElementsByTagName('h1')[0];
-const button = document.getElementsByTagName('button')[0];
 const footer = document.getElementsByTagName('footer')[0];
+const p = document.getElementsByTagName('p')[0];
 
+btChange.addEventListener('click', changeTheme)
+main.addEventListener('dblclick', changeFormationClasses)
 
 function changeTheme(){
-    body.classList.toggle('.darkMode');
-    footer.classList.add('.darkMode');
-    h1.innerText='aaaaaaaaa';
+    changeClasses()
+    changeText()
+}
+function changeClasses(){
+    body.classList.toggle('darkMode');
+    btChange.classList.toggle('darkMode');
+    footer.classList.toggle('darkMode');
+}
+function changeText(){
+    if(body.classList.contains('darkMode')){
+        h1.innerHTML='Dark Mode: <b>ON</b>'
+        btChange.innerHTML='Light Mode'
+    }else{
+        h1.innerHTML='Light Mode: <b>ON</b>'
+        btChange.innerHTML='Dark Mode'
+    }
+}
+function changeFormationClasses(){
+    body.classList.toggle('dbClick')
+    main.classList.toggle('dbClick')
+    footer.classList.toggle('dbClick')
+    p.classList.toggle('dbClick')
 
 }
-//mUDAR ESCRITA H1
-//mUDAR ESCRUTA BUTTON
-//Mudar cores(classList)
